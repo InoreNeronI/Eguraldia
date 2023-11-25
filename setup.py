0ec591e98c191d87f71289986c24e01e5331dfb1
mmozos <martinmozos@gmail.com>
2023-11-25 13:58:17 +0100

    Approach to fix "install_requires must be a string or list of strings containing valid project/version requirement specifiers"

--- setup.py
+++ setup.py
@@ -1,13 +1,21 @@
-# @see https://stackoverflow.com/a/16624700/16711967
-from pathlib import Path
-from pkg_resources import parse_requirements
+# @see https://stackoverflow.com/a/57191701/16711967
+from os import path
+try:
+    # pip >=20
+    from pip._internal.network.session import PipSession
+    from pip._internal.req import parse_requirements
+except ImportError:
+    try:
+        # 10.0.0 <= pip <= 19.3.1
+        from pip._internal.download import PipSession
+        from pip._internal.req import parse_requirements
+    except ImportError:
+        # pip <= 9.0.3
+        from pip.download import PipSession
+        from pip.req import parse_requirements
 from setuptools import setup
 
-with Path('requirements/prod.txt').open() as requirements_txt:
-    reqs = [
-        str(requirement)
-        for requirement
-        in parse_requirements(requirements_txt)]
+requirements = parse_requirements(path.join(path.dirname(__file__), 'requirements.txt'), session=PipSession())
 
 setup(
     name='WebUI',
@@ -24,6 +32,6 @@ setup(
     ],
     description='WebUI lets you create first class desktop applications in Python with HTML/CSS',
     long_description=open('README.md').read(),
-    install_requires=reqs,
-    python_requires='>=3.6',
+    install_requires=[str(requirement.requirement) for requirement in requirements],
+    python_requires='>=3.8',
 )
